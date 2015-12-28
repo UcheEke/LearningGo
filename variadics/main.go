952cf3a72465b81a0639fc3c9388bd2b8f91697c
Uche Eke <uche.eke@gmail.com>
2015-12-28 21:40:08 +0000

    variadic function basics

---
+++ variadics/main.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+
+// average takes in a variable number of float64 args, returning the simple mean of the
+// input
+func average(data ...float64) float64 {
+	if len(data) > 0 {
+		total := 0.0
+		for _, val := range data {
+			total += val
+		}
+		return total / float64(len(data))
+	} else {
+		return 0
+	}
+}
+
+func main() {
+	// Using a variadic function explicitly
+	fmt.Println("The average of the numbers [23,42,63,48,88,13,51]:", average(23,42,63,48,88,13,51))
+
+	// Using the variadic functions implicitly
+	data := []float64{23,42,63,48,88,13,51}
+	fmt.Println("The average of the numbers in data is", average(data...))
+}
